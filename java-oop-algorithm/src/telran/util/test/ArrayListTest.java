ff0ffa7306fef0754b00e677dd0d4c6bca7476bf
Svetlana Doskochinskaia <Light3308@gmail.com>
2023-03-31 14:12:29 +0300

    HW #3 ArraysList+Tests

--- java-oop-algorithm/src/telran/util/test/ArrayListTest.java
+++ java-oop-algorithm/src/telran/util/test/ArrayListTest.java
@@ -22,15 +22,38 @@ class ArraysListTest {
 	@Test
 	  public void testAddIndex() {
 		ArraysList<Integer> numbers = new ArraysList<>();
-		numbers.add(0, 5);
-        assertEquals(4, numbers.size());
-        assertEquals(5, numbers.get(0));
-       
-        numbers.add(2, 10);
-        assertEquals(5, numbers.size());
-        assertEquals(10, numbers.get(2));
+		for(int i= 0; i<20;i++) {
+			numbers.add(i);
+			}
+        assertEquals(19, numbers.size());
       
+        numbers.add(0, 100);
+      
+        assertEquals(100, numbers.get(0));
+        
+        assertEquals(20, numbers.size());
+        numbers.add(6, 200);
+        assertEquals(200, numbers.get(6));
     }
 	
+	@Test
+	  public void testRemote() {
+		ArraysList<Integer> numbers = new ArraysList<>();
+		for(int i= 0; i<20;i++) {
+		numbers.add(i);
+		}
+		int removed1 = numbers.remove(19);
+		int removed2 = numbers.remove(10);
+		int removed3 = numbers.remove(0);
+      assertEquals(4, numbers.size());
+      assertEquals(5, numbers.get(0));
+     
+      numbers.add(2, 10);
+      assertEquals(5, numbers.size());
+      assertEquals(19, removed1);
+      assertEquals(10, removed2);
+      assertEquals(0, removed3);
+    
+  }
 }
 
